8cee9726200a6b38783b42eb2f6df68df69e1943
abbas alkabbi <abbasalkabbi2003@gmail.com>
2024-01-13 00:48:59 +0300

    "Edit => Home"

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,21 @@
 import React from "react";
 import '../css/home.css'
+import { NavLink } from "react-router-dom";
 function Home() {
     return (
         <>
-            <main className="bg-dark-subtle home">
-                <h2>Welcome to the homepage!</h2>
-                <p>You can do this, I believe in you.</p>
+            <main className="bg-dark-subtle home d-flex flex-column justify-content-center align-items-center">
+                    <h1 className="text-center">
+                        Make your Quiz 
+                        <p>
+                            it's esay
+                        </p>
+                    </h1>
+                    <NavLink
+                    to='/login'
+                    className='btn btn-outline-success text-white link'>
+                        Take A Quiz
+                    </NavLink>
             </main>
         </>
     );
