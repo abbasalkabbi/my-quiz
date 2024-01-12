943d3e246258e2aee42dbb504ad5b6cfbe6b37ec
abbas alkabbi <abbasalkabbi2003@gmail.com>
2024-01-13 02:29:10 +0300

    "fix -> hero"

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 function Home() {
     return (
         <>
-            <main className="bg-dark-subtle home d-flex flex-column justify-content-center align-items-center">
+            <main className="bg-dark-subtle hero d-flex flex-column justify-content-center align-items-center">
                     <h1 className="text-center">
                         Make your Quiz 
                         <p>
