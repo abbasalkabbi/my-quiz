ac75ddb7dc17ae52cc6aff81923236640318e9b4
abbas alkabbi <abbasalkabbi2003@gmail.com>
2024-01-16 01:48:05 +0300

    "Add => Quiz"

---
+++ src/components/CardQuiz.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function CardQuiz(props){
+return(
+    <div class="col-sm-6 mb-3 col-lg-6">
+        <div class="card">
+            <div class="card-body d-flex justify-content-center align-items-center">
+                <p>How it is work</p>
+            </div>
+        </div>
+    </div>
+)
+}
+
+export default CardQuiz;
--- src/pages/Quiz.js
+++ src/pages/Quiz.js
@@ -2,24 +2,43 @@ import React from "react";
 import { useContext ,Component} from "react";
 import { useParams } from "react-router-dom";
 import Context from "../Context";
+import CardQuiz from "../components/CardQuiz";
 class Quiz extends Component{
     constructor(){
         super();
         this.state={
-            id:'',
+            id_quiz:'',
+            index:0,
+            quiz:'',
+            status:false,
         };
     }
      // Start componentDidMount
     componentDidMount(){
         let  {id_quiz}=this.props.params;
         this.setState({
-            id:id_quiz,
+            id_quiz:id_quiz,
         })
+    }
+    mapping(){
+        return(<h1>hi</h1>)
     }
     render(){
         return(
         <>
-        <h1>{this.state.id}</h1>
+        <div className="container">
+            <div className="row  ">
+                <p className="col-1 fs-4">1/10</p>
+                <h1 className="col-12 text-center ">Waht is that</h1>
+            </div>
+            <div class="row">
+                <CardQuiz/>
+                <CardQuiz/>
+                <CardQuiz/>
+                <CardQuiz/>
+            </div>
+        </div>
+        
         </>
         )
     }
