38cb13dc7e18f2e1185365c55165540db3f1cbe7
abbas alkabbi <abbasalkabbi2003@gmail.com>
2024-01-16 17:06:35 +0300

    "Add=> create_Question"

--- api/quiz.php
+++ api/quiz.php
@@ -9,8 +9,6 @@ class Quiz
     public $question;
     public $id_question;
     public $db;
-    
-
     private function SetId_Author($id_author){
         return $this->id_author=$id_author;
     }
@@ -32,11 +30,19 @@ class Quiz
         // create
         public function create_Quiz($quiz){
             $this->SetId_Author($quiz->id_author);
-            $this->SetId_Quiz($quiz->id_quiz);
+            // $this->SetId_Quiz($quiz->id_quiz);
             $this->Set_Name($quiz->quiz_name);
-            $SetQuiz=mysqli_query($this->db,"INSERT INTO `quiz`(`id_author`,`id_quiz`,`name`) VALUES ('$this->id_author','$this->id_quiz','$this->quiz_name')");
+            $SetQuiz=mysqli_query($this->db,"INSERT INTO `quiz`(`id_author`,`name`) VALUES ('$this->id_author','$this->quiz_name')");
             return false;
         }
         // end create
+        // create_Question
+        public function create_Question($question){
+        $this->SetId_Quiz($question->id_quiz);
+        $this->SetQuestion($question->question);
+        $Set_question=mysqli_query($this->db,"INSERT INTO `question`(`id_quiz`,`question`,) VALUES ('$this->id_quiz','$this->question')");
+        return false;
+        }
+        // end create_Question
 }
 ?>
