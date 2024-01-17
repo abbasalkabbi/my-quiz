909b75d2779cdfd152ffdb90c4207e1f49befaf3
abbas alkabbi <abbasalkabbi2003@gmail.com>
2024-01-17 23:42:39 +0300

    Quiz

--- src/components/CardQuiz.js
+++ src/components/CardQuiz.js
@@ -1,10 +1,10 @@
 import React from "react";
 
 
-function CardQuiz({answer,action,is_true,sty}){
+function CardQuiz({answer,action,is_true,sty,styl}){
 return(
     <div class="col-sm-6 mb-3 col-lg-6">
-        <div class={`card ${sty}`}>
+        <div class={`card ${sty} ${styl}`}>
             <div class="card-body d-flex justify-content-center align-items-center"  onClick={action}>
                 <p >{answer}</p>
             </div>
--- src/pages/Quiz.js
+++ src/pages/Quiz.js
@@ -112,12 +112,17 @@ export default function Quiz(){
     const[finshed,SetFinsed]=useState(false);
     const[q_finshed,Set_q_Finsed]=useState(false);
     const [is_next,SetNext]=useState(false);
+    const [count,SetCount]=useState(0);
+    const [title,SetTitle]=useState();
+
 //   // get user from api
     useEffect(()=>{
         fetch(`${link_quiz}`)
         .then(res=>res.json())
         .then(res =>{
+            console.log(res)
             SetQuestion(res.question)
+            SetCount(res.question.length)
             SetFinsed(true)
         })
         return (()=>false)
@@ -125,10 +130,12 @@ export default function Quiz(){
     useEffect(()=>{
         if(finshed ==true){
         const link_answer=url+`id_question=${question[id_answer-1].id_question}`;
+        SetTitle(question[id_answer-1].question)
         console.log(link_answer)
         fetch(`${link_answer}`)
         .then(res=>res.json())
         .then(res =>{
+            console.log(res)
             SetQuestion(res)
             Set_q_Finsed(true)
         })
@@ -140,18 +147,29 @@ const is_true=useCallback(()=>SetNext(true),[is_next]);
 const is_false=useCallback(()=>SetNext(false),[is_next]);
 function mapping(){
     if(q_finshed ==true){
-        return question.map(e=><CardQuiz answer={`${e.answer}`} action={e.is_true==1?is_true:is_false} is_true={`${e.is_true}`} sty={e.is_true==1&&is_next==true?"bg-success":''}/>)
+        return question.map(e=><CardQuiz answer={`${e.answer}`} action={e.is_true==1?is_true:is_false} is_true={`${e.is_true}`} sty={e.is_true==1&&is_next==true?"bg-success":''}
+    />)
     }
 }
 function Next(){
     if(is_next ==true){
-        return  (<li className='page-item'><a className="page-link" href={`/quiz/${id_quiz}/`+(parseInt(id_answer)+1)}>Next &#8250;</a></li>)
+        if(id_answer <count){
+            return  (<a className="page-link bg-primary p-2 bg-gradient text-bg-primary" href={`/quiz/${id_quiz}/`+(parseInt(id_answer)+1)}>Next &#8250;</a>)
+        }else{
+            return (<a className="page-link bg-primary p-2 bg-gradient text-bg-primary" href={`/quiz/${id_quiz}/`+(parseInt(id_answer)+1)}>Done &#8250;</a>)
+        }
     }
 }
     return(
-        <>
-        {Next()}
-        {mapping()}
-        </>
+        <div className="container">
+        <div className="d-flex justify-content-evenly align-items-center">
+            <p className="w-10">{id_answer}/{count}</p>
+            <p className="w-75 text-center fs-1">{title}</p>
+            {Next()}
+        </div>
+        <div className="row">
+        {mapping()} 
+        </div>
+        </div>
     )
 }
