6e97ed2e3faec62f6773ab40f501d1fde7a439e4
abbas alkabbi <abbasalkabbi2003@gmail.com>
2024-02-23 15:32:21 +0300

    404 user

--- src/pages/User.js
+++ src/pages/User.js
@@ -37,7 +37,18 @@ export default function User(){
         }
     }
     if(status ==false){
-      return(<></>)
+      return(<>
+        <div class="d-flex align-items-center justify-content-center vh-100">
+            <div class="text-center">
+                <h1 class="display-1 fw-bold">404</h1>
+                <p class="fs-3"> <span class="text-danger">Opps!</span> Page not found.</p>
+                <p class="lead">
+                    The User you’re looking for doesn’t exist.
+                  </p>
+                <a href="../" class="btn btn-primary">Go Home</a>
+            </div>
+        </div>
+      </>)
     }
     return(
     <>
